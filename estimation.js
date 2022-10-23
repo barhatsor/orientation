2f90578fa8f743d1f839121ddfbd3e513c76622d
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-23 14:04:12 +0300

    Update estimation.js

--- estimation.js
+++ estimation.js
@@ -4,7 +4,7 @@ function estimation(coord,currPos)
     //If the globcenter is not initialized:
     if(coord.lat==0 && coord.lon==0) return;
     
-    const z = -8;
+    const z = -8;//TBD??
 
     //Get the relative position to world center:
     let result = GetDirection(currPos, coord);//distance in [km]
@@ -12,16 +12,9 @@ function estimation(coord,currPos)
     let vrpos = new THREE.Vector3(result.x * Scale, z, result.y * Scale);
     let dist = CameraWrapper.position.distanceTo(vrpos);//[km*Scale]
 
-    //In [meters]
-    //If far than 5 [km] don't display
-    if(Math.floor(dist/Scale*1000) > 5000 && (InvokeAppOrigin=='PLAIN') )
-    {
-      if(DEBUG_LEVEL>0)
-      {
-        document.getElementById("ERRMSG").innerHTML = `${name} out of range ${Math.floor(dist/Scale*1000)}[m]`;
-      }
-      return;
-    }
+
+    let walkingTime = (((dist/Scale)/3)*60).toFixed(0);//in[minutes]
+    
 }
 
 
