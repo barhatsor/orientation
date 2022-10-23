714ae4a3fb47b0a49b0af913b466b517c6d6591d
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-23 14:02:14 +0300

    Update estimation.js

--- estimation.js
+++ estimation.js
@@ -1,2 +1,112 @@
 
-
+function estimation(coord,currPos)
+{
+    //If the globcenter is not initialized:
+    if(PosCoord.lat==0 && PosCoord.lon==0) return;
+    
+    const z = -8;
+
+    //Get the relative position to world center:
+    let result = GetDirection(WorldCenterPos, PosCoord);//distance in [km]
+
+    let vrpos = new THREE.Vector3(result.x * Scale, z, result.y * Scale);
+    let dist = CameraWrapper.position.distanceTo(vrpos);//[km*Scale]
+
+    //In [meters]
+    //If far than 5 [km] don't display
+    if(Math.floor(dist/Scale*1000) > 5000 && (InvokeAppOrigin=='PLAIN') )
+    {
+      if(DEBUG_LEVEL>0)
+      {
+        document.getElementById("ERRMSG").innerHTML = `${name} out of range ${Math.floor(dist/Scale*1000)}[m]`;
+      }
+      return;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
