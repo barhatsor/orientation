50a6068ae98460d0d8179f9f74a9c0af1a6c8e32
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-23 23:20:20 +0300

    Update estimation.js

--- estimation.js
+++ estimation.js
@@ -44,7 +44,7 @@ function getAspect(vrpos) {
   let dangle_z = laser_rot.z * 180 / 3.14;
 
   //return {y:dangle_y,x:dangle_x,z:dangle_z};//in degrees;
-  return laser.rotation;
+  return {y:laser.rotation.y * 180 / 3.14,x: laser.rotation.x * 180 / 3.14,z: laser.rotation.z * 180 / 3.14};
 }
 
 
