96b1f94c9a8bac6cd47394ee97614dabcf96a12f
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-23 15:09:11 +0300

    Update MotionHelper.js

--- MotionHelper.js
+++ MotionHelper.js
@@ -64,6 +64,7 @@ function InitMovement() {
   CenterPos.lon = 34.80578701716976;
   //GPS_sim.IsOn = false;
   
+  
   id_watch = navigator.geolocation.watchPosition(nav_geo_success, nav_geo_error, this.options);
 
   window.addEventListener('gps-coord-set', () => {
