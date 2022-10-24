b6f01c44a4443406ff66b35a70812592fd53fdf3
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-24 23:07:47 +0300

    Update MotionHelper.js

--- MotionHelper.js
+++ MotionHelper.js
@@ -179,4 +179,59 @@ function nav_geo_error(err) {
 }
 
 
+//Get direction from pt. of origin to target :
+//distance[km],[x,y]-in [km] and bearing[deg from north]
+//from pt of origin to target location:
+function GetDirection(Location_source, Location_target) {
+
+    let lat2 = Location_target.lat;
+    let lat1 = Location_source.lat;
+    let lon2 = Location_target.lon;
+    let lon1 = Location_source.lon;
+
+
+    let R = 6371; // [km]
+    let phi1 = THREE.Math.degToRad(lat1);
+    let phi2 = THREE.Math.degToRad(lat2);
+    let lambda1 = THREE.Math.degToRad(lon1);
+    let lambda2 = THREE.Math.degToRad(lon2);
+
+    let delta_phi = THREE.Math.degToRad(lat2 - lat1);
+    let delta_lambda = THREE.Math.degToRad(lon2 - lon1);
+
+    let a = Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) +
+        Math.cos(phi1) * Math.cos(phi2) *
+        Math.sin(delta_lambda / 2) * Math.sin(delta_lambda / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    let distance = d;//distance in km
+
+    //Calculate the bearing:
+    //where phi1, lambda1 is the start point, phi2,lambda2 the end point(delta_lambda is the difference in longitude)
+
+    let y = Math.sin(lambda2 - lambda1) * Math.cos(phi2);
+    let x = Math.cos(phi1) * Math.sin(phi2) -
+        Math.sin(phi1) * Math.cos(phi2) * Math.cos(lambda2 - lambda1);
+    let brng = THREE.Math.radToDeg(Math.atan2(y, x));
+
+    let bearing = brng;//bearing in [deg]
+
+    //Fix the non-relevant degrees:
+    bearing = (bearing + 360) % 360;
+
+    let brng_tmp = (brng + 360) % 360;
+    let x_ = distance * Math.cos(THREE.Math.degToRad(brng_tmp));// * THREE.Math.Deg2Rad);//km*Scale
+    let y_ = distance * Math.sin(THREE.Math.degToRad(brng_tmp));// * THREE.Math.Deg2Rad);//km*Scale
+
+    let result = {
+        distance_: distance,
+        bearing_: bearing,
+        x: x_,
+        y: y_,
+    };
+    return result;
+}
+
+
+
 
