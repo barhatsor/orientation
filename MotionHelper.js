559ba8913aff8aa437ca23c4042f0a5697cb4789
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-24 23:02:36 +0300

    Update MotionHelper.js

--- MotionHelper.js
+++ MotionHelper.js
@@ -8,7 +8,7 @@ var positionGPS = {lat: 0, lon: 0, last_lat: 0, last_lon: 0};//GPS position of t
 var positionXYZ = null;//VR position of the player in Vector3 format [km*Scale]
 
 var CenterPos = {lat: 0, lon: 0}; //Center of the world new Location(31.3365254f, 34.8968868f)
-var worldIsSet = false;
+var worldIsSet = false; //Indicates that the center of the VR world is set with GPS coordinates
 
 
 
