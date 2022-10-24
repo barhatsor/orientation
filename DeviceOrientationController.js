afea907a3b7889325ba9bf4722468d8cefb02f96
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-24 23:57:15 +0300

    Update DeviceOrientationController.js

--- DeviceOrientationController.js
+++ DeviceOrientationController.js
@@ -250,3 +250,38 @@ window.addEventListener("deviceorientationabsolute", function (event) {
     handleOrientationEvent(event);
 });
 
+
+//Calculate compass heading from orientation event data:
+//returns true north:
+function GetCompassHeading(alpha, beta, gamma) {
+
+    // Convert degrees to radians
+    var alphaRad = alpha * (Math.PI / 180);
+    var betaRad = beta * (Math.PI / 180);
+    var gammaRad = gamma * (Math.PI / 180);
+
+    // Calculate equation components
+    var cA = Math.cos(alphaRad);
+    var sA = Math.sin(alphaRad);
+    var sB = Math.sin(betaRad);
+    var cG = Math.cos(gammaRad);
+    var sG = Math.sin(gammaRad);
+
+    // Calculate A, B, C rotation components
+    var rA = -cA * sG - sA * sB * cG;
+    var rB = -sA * sG + cA * sB * cG;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(rA / rB);
+
+    // Convert from half unit circle to whole unit circle
+    if (rB < 0) {
+        compassHeading += Math.PI;
+    } else if (rA < 0) {
+        compassHeading += 2 * Math.PI;
+    }
+    // Convert radians to degrees
+    compassHeading *= 180 / Math.PI;
+
+    return compassHeading;
+}
