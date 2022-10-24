8807cf4ea7235644e48a592a15b2d59d5b5d591e
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-10-24 23:32:45 +0300

    Update DeviceOrientationController.js

--- DeviceOrientationController.js
+++ DeviceOrientationController.js
@@ -1,9 +1,6 @@
 /**
  * -------
- * threeVR (https://github.com/richtr/threeVR)
- * -------
  * W3C Device Orientation control (http://www.w3.org/TR/orientation-event/)
- * with manual user drag (rotate) and pinch (zoom) override handling
  * Author: Rich Tibbett (http://github.com/richtr)
  * License: The MIT License
  **/
@@ -24,17 +21,7 @@ var DeviceOrientationController = function ( object, domElement ) {
     this.deviceOrientation = {};
     this.screenOrientation = window.orientation || 0;
 
-    // Manual rotate override components
-    var startX = 0, startY = 0,
-        currentX = 0, currentY = 0,
-        scrollSpeedX, scrollSpeedY,
-        tmpQuat = new THREE.Quaternion();
-
-    // Manual zoom override components
-    var zoomStart = 1, zoomCurrent = 1,
-        zoomP1 = new THREE.Vector2(),
-        zoomP2 = new THREE.Vector2(),
-        tmpFOV;
+    
 
     var CONTROLLER_STATE = {
         AUTO: 0,
@@ -99,7 +86,210 @@ var DeviceOrientationController = function ( object, domElement ) {
         fireEvent( CONTROLLER_EVENT.CALIBRATE_COMPASS );
     }.bind( this );
 
-    this.onDocumentMouseDown = function ( event ) {
+    
+
+    var createQuaternion = function () {
+
+        var finalQuaternion = new THREE.Quaternion();
+
+        var deviceEuler = new THREE.Euler();
+
+        var screenTransform = new THREE.Quaternion();
+
+        var worldTransform = new THREE.Quaternion( - Math.sqrt(0.5), 0, 0, Math.sqrt(0.5) ); // - PI/2 around the x-axis
+
+        var minusHalfAngle = 0;
+
+        return function ( alpha, beta, gamma, screenOrientation ) {
+
+            deviceEuler.set( beta, alpha, - gamma, 'YXZ' );
+
+            finalQuaternion.setFromEuler( deviceEuler );
+
+            minusHalfAngle = - screenOrientation / 2;
+
+            screenTransform.set( 0, Math.sin( minusHalfAngle ), 0, Math.cos( minusHalfAngle ) );
+
+            finalQuaternion.multiply( screenTransform );
+
+            finalQuaternion.multiply( worldTransform );
+
+            return finalQuaternion;
+
+        }
+
+    }();
+
+    var createRotationMatrix = function () {
+
+        var finalMatrix = new THREE.Matrix4();
+
+        var deviceEuler = new THREE.Euler();
+        var screenEuler = new THREE.Euler();
+        var worldEuler = new THREE.Euler( - Math.PI / 2, 0, 0, 'YXZ' ); // - PI/2 around the x-axis
+
+        var screenTransform = new THREE.Matrix4();
+
+        var worldTransform = new THREE.Matrix4();
+        worldTransform.makeRotationFromEuler(worldEuler);
+
+        return function (alpha, beta, gamma, screenOrientation) {
+
+            deviceEuler.set( beta, alpha, - gamma, 'YXZ' );
+
+            finalMatrix.identity();
+
+            finalMatrix.makeRotationFromEuler( deviceEuler );
+
+            screenEuler.set( 0, - screenOrientation, 0, 'YXZ' );
+
+            screenTransform.identity();
+
+            screenTransform.makeRotationFromEuler( screenEuler );
+
+            finalMatrix.multiply( screenTransform );
+
+            finalMatrix.multiply( worldTransform );
+
+            return finalMatrix;
+
+        }
+
+    }();
+
+
+    //Gets rotation sensor data from browser event 
+    this.updateDeviceMove = function () {
+
+        var alpha, beta, gamma, orient;
+
+        var deviceMatrix;
+
+        return function () {
+
+            alpha  = THREE.Math.degToRad( this.deviceOrientation.alpha || 0 ); // Z
+            beta   = 0.1//THREE.Math.degToRad( this.deviceOrientation.beta  || 0 ); // X'
+            gamma  = 0.1//THREE.Math.degToRad( this.deviceOrientation.gamma || 0 ); // Y''
+            orient = 0.1//THREE.Math.degToRad( this.screenOrientation       || 0 ); // O
+
+            // only process non-zero 3-axis data
+            if ( alpha !== 0 && beta !== 0 && gamma !== 0) {
+
+                if ( this.useQuaternions ) {
+
+                    deviceQuat = createQuaternion( alpha, beta, gamma, orient );
+
+                } else {
+
+                    deviceMatrix = createRotationMatrix( alpha, beta, gamma, orient );
+
+                    deviceQuat.setFromRotationMatrix( deviceMatrix );
+
+                }
+
+                if ( this.freeze ) return;
+
+                //this.object.quaternion.slerp( deviceQuat, 0.07 ); // smoothing
+                this.object.quaternion.copy( deviceQuat );
+
+            }
+
+        };
+
+    }();
+
+    //External public function:
+    this.update = function () {
+        this.updateDeviceMove();
+    };
+
+
+    this.connect = function () {
+      
+        window.addEventListener( 'resize', this.constrainObjectFOV, false );
+
+        window.addEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
+
+        window.addEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
+
+        this.freeze = false;
+    };
+
+    this.disconnect = function () {
+        this.freeze = true;
+
+        window.removeEventListener( 'resize', this.constrainObjectFOV, false );
+
+        window.removeEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
+
+        window.removeEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
+
+        this.element.removeEventListener( 'mousedown', this.onDocumentMouseDown, false );
+        this.element.removeEventListener( 'touchstart', this.onDocumentTouchStart, false );
+    };
+
+};
+
+DeviceOrientationController.prototype = Object.create( THREE.EventDispatcher.prototype );
+
+
+
+
+
+//Call back for Rotation request:
+
+//Get compass reading (for IOS devices):
+window.addEventListener("deviceorientation", function (event) {
+    {
+        let compass_ = GetCompassHeading(event.webkitCompassHeading,
+            event.beta, event.gamma);
+        
+        //gamma is pitch.
+        //alpha is azimuth
+        //beta is roll
+        //hide the orientation button:
+        OrientationGrantedFlag=true;
+        
+        if(!isNaN(compass_) && compass_!=null) {
+            window.dispatchEvent(new CustomEvent('rotation-is-set',
+                {detail: {compass_reading: compass_}}));
+        }
+    }
+});
+
+
+//Get compass reading (For android devices):
+window.addEventListener("deviceorientationabsolute", function (event) {
+
+    function handleOrientationEvent(event) {
+      
+        // alpha: rotation around z-axis (azimuth)
+        let rotateDegrees = event.alpha;
+        let rotateDegreesIOS = event.absolute;
+        
+        // gamma: left to right (pitch)
+        let leftToRight = event.gamma;
+        // beta: front back motion (roll)
+        let frontToBack = event.beta;
+
+        let compass_heading = GetCompassHeading(event.alpha, event.beta, event.gamma);
+        
+        if(!isNaN(compass_heading) && compass_heading!=null) {
+                      
+            //Set the event for compass reading is ready and reliable:
+            window.dispatchEvent(new CustomEvent('rotation-is-set',
+            {detail: {compass_reading: compass_heading}}));
+          }
+        }
+    
+    handleOrientationEvent(event);
+});
+
+
+
+
+
+/*this.onDocumentMouseDown = function ( event ) {
         if ( this.enableManualDrag !== true ) return;
 
         event.preventDefault();
@@ -122,9 +312,9 @@ var DeviceOrientationController = function ( object, domElement ) {
 
         fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'start' );
         fireEvent( CONTROLLER_EVENT.ROTATE_CONTROL + 'start' );
-    }.bind( this );
+    }.bind( this );*/
 
-    this.onDocumentMouseMove = function ( event ) {
+    /*this.onDocumentMouseMove = function ( event ) {
         currentX = event.pageX;
         currentY = event.pageY;
     }.bind( this );
@@ -233,78 +423,9 @@ var DeviceOrientationController = function ( object, domElement ) {
             fireEvent( CONTROLLER_EVENT.ZOOM_CONTROL + 'end' );
 
         }
-    }.bind( this );
+    }.bind( this );*/
 
-    var createQuaternion = function () {
-
-        var finalQuaternion = new THREE.Quaternion();
-
-        var deviceEuler = new THREE.Euler();
-
-        var screenTransform = new THREE.Quaternion();
-
-        var worldTransform = new THREE.Quaternion( - Math.sqrt(0.5), 0, 0, Math.sqrt(0.5) ); // - PI/2 around the x-axis
-
-        var minusHalfAngle = 0;
-
-        return function ( alpha, beta, gamma, screenOrientation ) {
-
-            deviceEuler.set( beta, alpha, - gamma, 'YXZ' );
-
-            finalQuaternion.setFromEuler( deviceEuler );
-
-            minusHalfAngle = - screenOrientation / 2;
-
-            screenTransform.set( 0, Math.sin( minusHalfAngle ), 0, Math.cos( minusHalfAngle ) );
-
-            finalQuaternion.multiply( screenTransform );
-
-            finalQuaternion.multiply( worldTransform );
-
-            return finalQuaternion;
-
-        }
-
-    }();
-
-    var createRotationMatrix = function () {
-
-        var finalMatrix = new THREE.Matrix4();
-
-        var deviceEuler = new THREE.Euler();
-        var screenEuler = new THREE.Euler();
-        var worldEuler = new THREE.Euler( - Math.PI / 2, 0, 0, 'YXZ' ); // - PI/2 around the x-axis
-
-        var screenTransform = new THREE.Matrix4();
-
-        var worldTransform = new THREE.Matrix4();
-        worldTransform.makeRotationFromEuler(worldEuler);
-
-        return function (alpha, beta, gamma, screenOrientation) {
-
-            deviceEuler.set( beta, alpha, - gamma, 'YXZ' );
-
-            finalMatrix.identity();
-
-            finalMatrix.makeRotationFromEuler( deviceEuler );
-
-            screenEuler.set( 0, - screenOrientation, 0, 'YXZ' );
-
-            screenTransform.identity();
-
-            screenTransform.makeRotationFromEuler( screenEuler );
-
-            finalMatrix.multiply( screenTransform );
-
-            finalMatrix.multiply( worldTransform );
-
-            return finalMatrix;
-
-        }
-
-    }();
-
-    this.updateManualMove = function () {
+    /*this.updateManualMove = function () {
 
         var lat, lon;
         var phi, theta;
@@ -387,137 +508,4 @@ var DeviceOrientationController = function ( object, domElement ) {
 
         };
 
-    }();
-
-    this.updateDeviceMove = function () {
-
-        var alpha, beta, gamma, orient;
-
-        var deviceMatrix;
-
-        return function () {
-
-            alpha  = THREE.Math.degToRad( this.deviceOrientation.alpha || 0 ); // Z
-            beta   = 0.1//THREE.Math.degToRad( this.deviceOrientation.beta  || 0 ); // X'
-            gamma  = 0.1//THREE.Math.degToRad( this.deviceOrientation.gamma || 0 ); // Y''
-            orient = 0.1//THREE.Math.degToRad( this.screenOrientation       || 0 ); // O
-
-            // only process non-zero 3-axis data
-            if ( alpha !== 0 && beta !== 0 && gamma !== 0) {
-
-                if ( this.useQuaternions ) {
-
-                    deviceQuat = createQuaternion( alpha, beta, gamma, orient );
-
-                } else {
-
-                    deviceMatrix = createRotationMatrix( alpha, beta, gamma, orient );
-
-                    deviceQuat.setFromRotationMatrix( deviceMatrix );
-
-                }
-
-                if ( this.freeze ) return;
-
-                //this.object.quaternion.slerp( deviceQuat, 0.07 ); // smoothing
-                this.object.quaternion.copy( deviceQuat );
-
-            }
-
-        };
-
-    }();
-
-    this.update = function () {
-        this.updateDeviceMove();
-
-        if ( appState !== CONTROLLER_STATE.AUTO ) {
-            this.updateManualMove();
-        }
-    };
-
-    this.connect = function () {
-        window.addEventListener( 'resize', this.constrainObjectFOV, false );
-
-        window.addEventListener( 'orientationchange', this.onScreenOrientationChange, false );
-        window.addEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
-
-        window.addEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
-
-        this.element.addEventListener( 'mousedown', this.onDocumentMouseDown, false );
-        this.element.addEventListener( 'touchstart', this.onDocumentTouchStart, false );
-
-        this.freeze = false;
-    };
-
-    this.disconnect = function () {
-        this.freeze = true;
-
-        window.removeEventListener( 'resize', this.constrainObjectFOV, false );
-
-        window.removeEventListener( 'orientationchange', this.onScreenOrientationChange, false );
-        window.removeEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
-
-        window.removeEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
-
-        this.element.removeEventListener( 'mousedown', this.onDocumentMouseDown, false );
-        this.element.removeEventListener( 'touchstart', this.onDocumentTouchStart, false );
-    };
-
-};
-
-DeviceOrientationController.prototype = Object.create( THREE.EventDispatcher.prototype );
-
-
-
-
-
-//Call back for Rotation request:
-
-//For IOS devices:
-window.addEventListener("deviceorientation", function (event) {
-    {
-        let compass_ = GetCompassHeading(event.webkitCompassHeading,
-            event.beta, event.gamma);
-        
-        //gamma is pitch.
-        //alpha is azimuth
-        //beta is roll
-        //hide the orientation button:
-        OrientationGrantedFlag=true;
-        
-        if(!isNaN(compass_) && compass_!=null) {
-            window.dispatchEvent(new CustomEvent('rotation-is-set',
-                {detail: {compass_reading: compass_}}));
-        }
-    }
-});
-
-//For android devices:
-window.addEventListener("deviceorientationabsolute", function (event) {
-
-    function handleOrientationEvent(event) {
-      
-        // alpha: rotation around z-axis (azimuth)
-        let rotateDegrees = event.alpha;
-        let rotateDegreesIOS = event.absolute;
-        
-        // gamma: left to right (pitch)
-        let leftToRight = event.gamma;
-        // beta: front back motion (roll)
-        let frontToBack = event.beta;
-
-        let compass_heading = GetCompassHeading(event.alpha, event.beta, event.gamma);
-        
-        if(!isNaN(compass_heading) && compass_heading!=null) {
-                      
-            //Set the event for compass reading is ready and reliable:
-            window.dispatchEvent(new CustomEvent('rotation-is-set',
-            {detail: {compass_reading: compass_heading}}));
-          }
-        }
-    
-    handleOrientationEvent(event);
-});
-
-
+    }();*/
