e639e6f72581358816f686c4d9e852d9cb4bbe95
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-10-27 21:50:30 +0300

    Update DeviceOrientationController.js

--- DeviceOrientationController.js
+++ DeviceOrientationController.js
@@ -1,238 +1,64 @@
 /**
  * -------
- * threeVR (https://github.com/richtr/threeVR)
- * -------
  * W3C Device Orientation control (http://www.w3.org/TR/orientation-event/)
- * with manual user drag (rotate) and pinch (zoom) override handling
  * Author: Rich Tibbett (http://github.com/richtr)
  * License: The MIT License
- **/
 
+ * Added: Compass readings for Android and iOS
+ **/
 
 var DeviceOrientationController = function ( object, domElement ) {
 
-    this.object = object;
-    this.element = domElement || document;
+    
+    this.object = object; // object is a camera give as an argument, 
+                          //the controller will copy the updated rotation to camera
+    
+    this.element = domElement || document; //if null points to the document
 
     this.freeze = true;
 
-    this.enableManualDrag = true; // enable manual user drag override control by default
-    this.enableManualZoom = true; // enable manual user zoom override control by default
-
     this.useQuaternions = true; // use quaternions for orientation calculation by default
 
-    this.deviceOrientation = {};
-    this.screenOrientation = window.orientation || 0;
+    this.deviceOrientation = {}; //Holds the updated orientation sensor data from the browser
+     
+    this.screenOrientation = window.orientation || 0; //depricated
 
-    // Manual rotate override components
-    var startX = 0, startY = 0,
-        currentX = 0, currentY = 0,
-        scrollSpeedX, scrollSpeedY,
-        tmpQuat = new THREE.Quaternion();
 
-    // Manual zoom override components
-    var zoomStart = 1, zoomCurrent = 1,
-        zoomP1 = new THREE.Vector2(),
-        zoomP2 = new THREE.Vector2(),
-        tmpFOV;
-
-    var CONTROLLER_STATE = {
-        AUTO: 0,
-        MANUAL_ROTATE: 1,
-        MANUAL_ZOOM: 2
-    };
-
-    var appState = CONTROLLER_STATE.AUTO;
-
-    var CONTROLLER_EVENT = {
-        CALIBRATE_COMPASS:  'compassneedscalibration',
-        SCREEN_ORIENTATION: 'orientationchange',
-        MANUAL_CONTROL:     'userinteraction', // userinteractionstart, userinteractionend
-        ZOOM_CONTROL:       'zoom',            // zoomstart, zoomend
-        ROTATE_CONTROL:     'rotate',          // rotatestart, rotateend
-    };
-
-    // Consistent Object Field-Of-View fix components
+    // Consistent Object Field-Of-View fix components (in case of window size change)
     var startClientHeight = window.innerHeight,
         startFOVFrustrumHeight = 2000 * Math.tan( THREE.Math.degToRad( ( this.object.fov || 75 ) / 2 ) ),
-        relativeFOVFrustrumHeight, relativeVerticalFOV;
+        relativeFOVFrustrumHeight, 
+        relativeVerticalFOV;
 
     var deviceQuat = new THREE.Quaternion();
+    
 
-    var fireEvent = function () {
-        var eventData;
 
-        return function ( name ) {
-            eventData = arguments || {};
 
-            eventData.type = name;
-            eventData.target = this;
 
-            this.dispatchEvent( eventData );
-        }.bind( this );
-    }.bind( this )();
-
+    //Function constrains this object field of view:
     this.constrainObjectFOV = function () {
+      
         relativeFOVFrustrumHeight = startFOVFrustrumHeight * ( window.innerHeight / startClientHeight );
 
         relativeVerticalFOV = THREE.Math.radToDeg( 2 * Math.atan( relativeFOVFrustrumHeight / 2000 ) );
 
         this.object.fov = relativeVerticalFOV;
+        
     }.bind( this );
 
+    
+    
+    //Get the sensor data form browser event:
     this.onDeviceOrientationChange = function ( event ) {
+      
         this.deviceOrientation = event;
 
     }.bind( this );
 
-    this.onScreenOrientationChange = function () {
-        this.screenOrientation = window.orientation || 0;
 
-        fireEvent( CONTROLLER_EVENT.SCREEN_ORIENTATION );
-    }.bind( this );
 
-    this.onCompassNeedsCalibration = function ( event ) {
-        event.preventDefault();
-
-        fireEvent( CONTROLLER_EVENT.CALIBRATE_COMPASS );
-    }.bind( this );
-
-    this.onDocumentMouseDown = function ( event ) {
-        if ( this.enableManualDrag !== true ) return;
-
-        event.preventDefault();
-
-        appState = CONTROLLER_STATE.MANUAL_ROTATE;
-
-        this.freeze = true;
-
-        tmpQuat.copy( this.object.quaternion );
-
-        startX = currentX = event.pageX;
-        startY = currentY = event.pageY;
-
-        // Set consistent scroll speed based on current viewport width/height
-        scrollSpeedX = ( 1200 / window.innerWidth ) * 0.2;
-        scrollSpeedY = ( 800 / window.innerHeight ) * 0.2;
-
-        this.element.addEventListener( 'mousemove', this.onDocumentMouseMove, false );
-        this.element.addEventListener( 'mouseup', this.onDocumentMouseUp, false );
-
-        fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'start' );
-        fireEvent( CONTROLLER_EVENT.ROTATE_CONTROL + 'start' );
-    }.bind( this );
-
-    this.onDocumentMouseMove = function ( event ) {
-        currentX = event.pageX;
-        currentY = event.pageY;
-    }.bind( this );
-
-    this.onDocumentMouseUp = function ( event ) {
-        this.element.removeEventListener( 'mousemove', this.onDocumentMouseMove, false );
-        this.element.removeEventListener( 'mouseup', this.onDocumentMouseUp, false );
-
-        appState = CONTROLLER_STATE.AUTO;
-
-        this.freeze = false;
-
-        fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'end' );
-        fireEvent( CONTROLLER_EVENT.ROTATE_CONTROL + 'end' );
-    }.bind( this );
-
-    this.onDocumentTouchStart = function ( event ) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        switch ( event.touches.length ) {
-            case 1: // ROTATE
-                if ( this.enableManualDrag !== true ) return;
-
-                appState = CONTROLLER_STATE.MANUAL_ROTATE;
-
-                this.freeze = true;
-
-                tmpQuat.copy( this.object.quaternion );
-
-                startX = currentX = event.touches[ 0 ].pageX;
-                startY = currentY = event.touches[ 0 ].pageY;
-
-                // Set consistent scroll speed based on current viewport width/height
-                scrollSpeedX = ( 1200 / window.innerWidth ) * 0.1;
-                scrollSpeedY = ( 800 / window.innerHeight ) * 0.1;
-
-                this.element.addEventListener( 'touchmove', this.onDocumentTouchMove, false );
-                this.element.addEventListener( 'touchend', this.onDocumentTouchEnd, false );
-
-                fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'start' );
-                fireEvent( CONTROLLER_EVENT.ROTATE_CONTROL + 'start' );
-
-                break;
-
-            case 2: // ZOOM
-                if ( this.enableManualZoom !== true ) return;
-
-                appState = CONTROLLER_STATE.MANUAL_ZOOM;
-
-                this.freeze = true;
-
-                tmpFOV = this.object.fov;
-
-                zoomP1.set( event.touches[ 0 ].pageX, event.touches[ 0 ].pageY );
-                zoomP2.set( event.touches[ 1 ].pageX, event.touches[ 1 ].pageY );
-
-                zoomStart = zoomCurrent = zoomP1.distanceTo( zoomP2 );
-
-                this.element.addEventListener( 'touchmove', this.onDocumentTouchMove, false );
-                this.element.addEventListener( 'touchend', this.onDocumentTouchEnd, false );
-
-                fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'start' );
-                fireEvent( CONTROLLER_EVENT.ZOOM_CONTROL + 'start' );
-
-                break;
-        }
-    }.bind( this );
-
-    this.onDocumentTouchMove = function ( event ) {
-        switch( event.touches.length ) {
-            case 1:
-                currentX = event.touches[ 0 ].pageX;
-                currentY = event.touches[ 0 ].pageY;
-                break;
-
-            case 2:
-                zoomP1.set( event.touches[ 0 ].pageX, event.touches[ 0 ].pageY );
-                zoomP2.set( event.touches[ 1 ].pageX, event.touches[ 1 ].pageY );
-                break;
-        }
-    }.bind( this );
-
-    this.onDocumentTouchEnd = function ( event ) {
-        this.element.removeEventListener( 'touchmove', this.onDocumentTouchMove, false );
-        this.element.removeEventListener( 'touchend', this.onDocumentTouchEnd, false );
-
-        if ( appState === CONTROLLER_STATE.MANUAL_ROTATE ) {
-
-            appState = CONTROLLER_STATE.AUTO; // reset control state
-
-            this.freeze = false;
-
-            fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'end' );
-            fireEvent( CONTROLLER_EVENT.ROTATE_CONTROL + 'end' );
-
-        } else if ( appState === CONTROLLER_STATE.MANUAL_ZOOM ) {
-
-            this.constrainObjectFOV(); // re-instate original object FOV
-
-            appState = CONTROLLER_STATE.AUTO; // reset control state
-
-            this.freeze = false;
-
-            fireEvent( CONTROLLER_EVENT.MANUAL_CONTROL + 'end' );
-            fireEvent( CONTROLLER_EVENT.ZOOM_CONTROL + 'end' );
-
-        }
-    }.bind( this );
-
+    //Function which creates a new quanterion based on on rotation readings from device:
     var createQuaternion = function () {
 
         var finalQuaternion = new THREE.Quaternion();
@@ -265,17 +91,21 @@ var DeviceOrientationController = function ( object, domElement ) {
 
     }();
 
+    //Function creates rotation matrix:
     var createRotationMatrix = function () {
 
         var finalMatrix = new THREE.Matrix4();
 
         var deviceEuler = new THREE.Euler();
+        
         var screenEuler = new THREE.Euler();
+        
         var worldEuler = new THREE.Euler( - Math.PI / 2, 0, 0, 'YXZ' ); // - PI/2 around the x-axis
 
         var screenTransform = new THREE.Matrix4();
 
         var worldTransform = new THREE.Matrix4();
+        
         worldTransform.makeRotationFromEuler(worldEuler);
 
         return function (alpha, beta, gamma, screenOrientation) {
@@ -302,91 +132,8 @@ var DeviceOrientationController = function ( object, domElement ) {
 
     }();
 
-    this.updateManualMove = function () {
 
-        var lat, lon;
-        var phi, theta;
-
-        var rotation = new THREE.Euler( 0, 0, 0, 'YXZ' );
-
-        var rotQuat = new THREE.Quaternion();
-        var objQuat = new THREE.Quaternion();
-
-        var tmpZ, objZ, realZ;
-
-        var zoomFactor, minZoomFactor = 1; // maxZoomFactor = Infinity
-
-        return function () {
-
-            objQuat.copy( tmpQuat );
-
-            if ( appState === CONTROLLER_STATE.MANUAL_ROTATE ) {
-
-                lat = ( startY - currentY ) * scrollSpeedY;
-                lon = ( startX - currentX ) * scrollSpeedX;
-
-                phi	 = THREE.Math.degToRad( lat );
-                theta = THREE.Math.degToRad( lon );
-
-                rotQuat.set( 0, Math.sin( theta / 2 ), 0, Math.cos( theta / 2 ) );
-
-                objQuat.multiply( rotQuat );
-
-                rotQuat.set( Math.sin( phi / 2 ), 0, 0, Math.cos( phi / 2 ) );
-
-                objQuat.multiply( rotQuat );
-
-                // Remove introduced z-axis rotation and add device's current z-axis rotation
-
-                tmpZ  = rotation.setFromQuaternion( tmpQuat, 'YXZ' ).z;
-                objZ  = rotation.setFromQuaternion( objQuat, 'YXZ' ).z;
-                realZ = rotation.setFromQuaternion( deviceQuat || tmpQuat, 'YXZ' ).z;
-
-                rotQuat.set( 0, 0, Math.sin( ( realZ - tmpZ  ) / 2 ), Math.cos( ( realZ - tmpZ ) / 2 ) );
-
-                tmpQuat.multiply( rotQuat );
-
-                rotQuat.set( 0, 0, Math.sin( ( realZ - objZ  ) / 2 ), Math.cos( ( realZ - objZ ) / 2 ) );
-
-                objQuat.multiply( rotQuat );
-
-                this.object.quaternion.copy( objQuat );
-
-            } else if ( appState === CONTROLLER_STATE.MANUAL_ZOOM ) {
-
-                zoomCurrent = zoomP1.distanceTo( zoomP2 );
-
-                zoomFactor = zoomStart / zoomCurrent;
-
-                if ( zoomFactor <= minZoomFactor ) {
-
-                    this.object.fov = tmpFOV * zoomFactor;
-
-                    this.object.updateProjectionMatrix();
-
-                }
-
-                // Add device's current z-axis rotation
-
-                if ( deviceQuat ) {
-
-                    tmpZ  = rotation.setFromQuaternion( tmpQuat, 'YXZ' ).z;
-                    realZ = rotation.setFromQuaternion( deviceQuat, 'YXZ' ).z;
-
-                    rotQuat.set( 0, 0, Math.sin( ( realZ - tmpZ ) / 2 ), Math.cos( ( realZ - tmpZ ) / 2 ) );
-
-                    tmpQuat.multiply( rotQuat );
-
-                    this.object.quaternion.copy( tmpQuat );
-
-                }
-
-            }
-
-        };
-
-    }();
-
+    //Gets rotation sensor data from browser event 
     this.updateDeviceMove = function () {
 
         var alpha, beta, gamma, orient;
@@ -394,11 +141,12 @@ var DeviceOrientationController = function ( object, domElement ) {
         var deviceMatrix;
 
         return function () {
-
+            
+            //Ignore all but azimuth: (for magic "compass-lazer" requirement)
             alpha  = THREE.Math.degToRad( this.deviceOrientation.alpha || 0 ); // Z
-            beta   = THREE.Math.degToRad( this.deviceOrientation.beta  || 0 ); // X'
-            gamma  = THREE.Math.degToRad( this.deviceOrientation.gamma || 0 ); // Y''
-            orient = THREE.Math.degToRad( this.screenOrientation       || 0 ); // O
+            beta   = 0.1//THREE.Math.degToRad( this.deviceOrientation.beta  || 0 ); // X'
+            gamma  = 0.1//THREE.Math.degToRad( this.deviceOrientation.gamma || 0 ); // Y''
+            orient = 0.1//THREE.Math.degToRad( this.screenOrientation       || 0 ); // O
 
             // only process non-zero 3-axis data
             if ( alpha !== 0 && beta !== 0 && gamma !== 0) {
@@ -414,54 +162,123 @@ var DeviceOrientationController = function ( object, domElement ) {
                     deviceQuat.setFromRotationMatrix( deviceMatrix );
 
                 }
-
-                if ( this.freeze ) return;
-
-                //this.object.quaternion.slerp( deviceQuat, 0.07 ); // smoothing
+                
+                //If event listeners are removed do nothing:
+                if ( this.freeze ) 
+                  return;
+                  
+                //Copy the rotation to the camera (as quanterion):
                 this.object.quaternion.copy( deviceQuat );
-
+                //this.object.quaternion.slerp( deviceQuat, 0.07 ); // smoothing
             }
 
         };
 
     }();
 
+    //Public function to update rotation values:
     this.update = function () {
         this.updateDeviceMove();
-
-        if ( appState !== CONTROLLER_STATE.AUTO ) {
-            this.updateManualMove();
-        }
     };
 
+
     this.connect = function () {
+      
         window.addEventListener( 'resize', this.constrainObjectFOV, false );
 
-        window.addEventListener( 'orientationchange', this.onScreenOrientationChange, false );
         window.addEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
-
-        window.addEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
-
-        this.element.addEventListener( 'mousedown', this.onDocumentMouseDown, false );
-        this.element.addEventListener( 'touchstart', this.onDocumentTouchStart, false );
 
         this.freeze = false;
     };
 
+
     this.disconnect = function () {
+      
         this.freeze = true;
 
         window.removeEventListener( 'resize', this.constrainObjectFOV, false );
 
-        window.removeEventListener( 'orientationchange', this.onScreenOrientationChange, false );
         window.removeEventListener( 'deviceorientation', this.onDeviceOrientationChange, false );
 
-        window.removeEventListener( 'compassneedscalibration', this.onCompassNeedsCalibration, false );
-
-        this.element.removeEventListener( 'mousedown', this.onDocumentMouseDown, false );
-        this.element.removeEventListener( 'touchstart', this.onDocumentTouchStart, false );
     };
 
 };
 
 DeviceOrientationController.prototype = Object.create( THREE.EventDispatcher.prototype );
+
+
+
+// get compass readings (iOS)
+window.addEventListener('deviceorientation', (e) => {
+
+  handleOrientationEvent(e.webkitCompassHeading, e.beta, e.gamma);
+  
+});
+
+// get compass readings (Android)
+window.addEventListener('deviceorientationabsolute', (e) => {
+  
+  handleOrientationEvent(e.alpha, e.beta, e.gamma);
+
+});
+
+
+/* 
+ * alpha is azimuth [left to right]
+ * beta is roll
+ * gamma is pitch
+ */
+function handleOrientationEvent(alpha, beta, gamma) {
+
+  let compassHeading = getCompassHeading(alpha, beta, gamma);
+
+  if (!isNaN(compassHeading) && compassHeading != null) {
+
+    // compass readings are ready and reliable
+    window.dispatchEvent(new CustomEvent('compass-event', {
+      detail: {
+        compass_reading: compassHeading
+      }
+    }));
+
+  }
+  
+}
+
+
+// calculate compass heading from orientation event data
+// returns true north
+function getCompassHeading(alpha, beta, gamma) {
+
+  // convert degrees to radians
+  const alphaRad = alpha * (Math.PI / 180);
+  const betaRad = beta * (Math.PI / 180);
+  const gammaRad = gamma * (Math.PI / 180);
+
+  // calculate equation components
+  const cA = Math.cos(alphaRad);
+  const sA = Math.sin(alphaRad);
+  const sB = Math.sin(betaRad);
+  const cG = Math.cos(gammaRad);
+  const sG = Math.sin(gammaRad);
+
+  // calculate A, B, C rotation components
+  const rA = -cA * sG - sA * sB * cG;
+  const rB = -sA * sG + cA * sB * cG;
+
+  // calculate compass heading
+  let compassHeading = Math.atan(rA / rB);
+
+  // convert half unit circle to whole unit circle
+  if (rB < 0) {
+    compassHeading += Math.PI;
+  } else if (rA < 0) {
+    compassHeading += 2 * Math.PI;
+  }
+  
+  // convert radians to degrees
+  compassHeading *= 180 / Math.PI;
+
+  return compassHeading;
+  
+}
