d34582043380a8d790bebe3616d92c8b99ec8fa8
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-16 16:26:51 +0200

    Update estimation.js

--- estimation.js
+++ estimation.js
@@ -1,24 +1,24 @@
 
-//Estimate time to target in minutes: 
-//(assuming walking spped is WALK_SPEED km/h)
+// get direction in [deg] and time in [min] to destination
+// (assuming WALK_SPEED [km/h])
 function estimation(coord, currPos) {
   
-  //If the globcenter is not initialized:
+  // if world center is not initialized (GPS not initalized)
   if (coord.lat == 0 && coord.lon == 0)
     return;
 
-  const WALK_SPEED = 3; //km/h
+  const WALK_SPEED = 3; // [km/h]
 
-  const z = -8; //see the z of the player position (which is set to be z=4 in motionHelper module)
+  const z = -8; // set z pos of player (which is z = 4 in motionHelper module)
 
-  //Get the relative position to world center:
-  let result = GetDirection(currPos, coord); //distance in [km]
+  // convert lat, lng to point in 3d space (relative position to world center)
+  let result = GetDirection(currPos, coord); // in [km]
 
   let vrpos = new THREE.Vector3(result.x * Scale, z, result.y * Scale);
   
-  let dist = CameraWrapper.position.distanceTo(vrpos); //[km*Scale]
+  let dist = CameraWrapper.position.distanceTo(vrpos); // [km*Scale]
 
-  let walkingTime = (((dist / Scale) / WALK_SPEED) * 60).toFixed(1); //in[minutes]
+  let walkingTime = (((dist / Scale) / WALK_SPEED) * 60).toFixed(1); // in [min]
 
   return { walkDist: walkingTime, aspect: getAspect(vrpos) };
 
@@ -26,43 +26,44 @@ function estimation(coord, currPos) {
 
 
 
-//Retrieves relative angle to camera view 
-//given relative position in 3D space vrpos:
+// retrieves angle in deg from camera view 
+// to a relative point in 3D space
 function getAspect(vrpos) {
 
-  //"camera" is global variable 
-  //which defines the current point view of the player
+  // "camera" is a global variable 
+  // which defines the current view of the player
   camera.updateMatrix();
   
   camera.updateMatrixWorld();
   
   // 3D point to check
-  var pos = new THREE.Vector3(vrpos.x, vrpos.y, vrpos.z);
+  let pos = new THREE.Vector3(vrpos.x, vrpos.y, vrpos.z);
   
-
+  
   pos.x = -vrpos.x * Scale;
   
-  pos.y = (CameraWrapper.position.y) + Math.floor(5*(Math.random())); //height of the point relative to plane xz
+  pos.y = CameraWrapper.position.y; /*+ Math.floor(5*(Math.random()))*/ // height of the point relative to plane xz
   
   pos.z = vrpos.y * Scale;
   
-  //Create laser to the object to measure the delta angle:
+  // create a laser to object to measure the delta angle
   laser.lookAt(pos);
   
   let laser_rot = new THREE.Vector3(laser.rotation.x, 
                                     laser.rotation.y, laser.rotation.z);
 
   let ly = laser_rot.y * 180 / 3.14;
-  let lx = laser_rot.x * 180 / 3.14; //pitch: [-90,..,-180,180,..,90]
+  let lx = laser_rot.x * 180 / 3.14; // pitch: [-90,..,-180,180,..,90]
   let lz = laser_rot.z * 180 / 3.14;
 
-  return {y:ly,x:lx,z:lz};//in degrees;
+  return {y: ly, x: lx, z: lz}; // in [deg]
   
 }
 
 
 
-
+// development test function @@
+/*
 function testVitals() {
   
   //Check GPS data availability:
@@ -87,8 +88,6 @@ function testVitals() {
     lon: 34.896735
   };
   
-  
-  
   //kosta
   const park2 = 
   { 
@@ -107,3 +106,5 @@ function testVitals() {
   console.log('Testing estimation: walk distance in minutes' + res.walkDist + ', aspect[deg]:'+JSON.stringify(res.aspect) );
 
 }
+*/
+
