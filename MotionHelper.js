63d63b0623fab3e7f3fe352adc21a4cbddbc60c0
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-11-17 18:32:32 +0200

    Update MotionHelper.js

--- MotionHelper.js
+++ MotionHelper.js
@@ -175,7 +175,7 @@ function animatePos(vecA, vecB, numSteps) {
 
 // get direction and distance from origin { lat, lon } to target { lat, lon }
 // returns bearing [deg from north], distance in [km] and X, Y in [km]
-function getDirection(origin, target) { // @@ uses this in two places: here, and in estimation.js first function
+function getDirection(origin, target) {
   
   const [lat1, lon1] = [origin.lat, origin.lon];
   const [lat2, lon2] = [target.lat, target.lon];
